3dee2d17c24c118b14f22b247db5367179515388
Gerald Wood <gerald.wood@lknlife.com>
2019-04-18 09:58:45 -0400

    A Sample Coloring Book

---
+++ HelpTheKids/DrawingView.swift
@@ -0,0 +1,105 @@
+//
+//  DrawingView.swift
+//  HelpTheKids
+//
+//  Created by Gerald Wood on 4/18/19.
+//  Copyright © 2019 Gerald Wood. All rights reserved.
+//
+
+import UIKit
+
+protocol DrawingProtocol
+{
+    func fingerDidMoveToOutSideThePath(str:String)
+}
+
+class DrawingView: UIView {
+
+    var bezierPath:UIBezierPath?, path:UIBezierPath?
+    var startPoint:CGPoint?
+    var pts = [CGPoint?](repeating: nil, count: 5)
+    var ctr = 0;
+    var delegate:DrawingProtocol! = nil
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        path = UIBezierPath()
+        UIColor.red.setStroke()
+        UIGraphicsBeginImageContext(self.frame.size)
+    }
+    
+    func createShape()
+    {
+        bezierPath = UIBezierPath()
+        bezierPath?.move(to: CGPoint(x: 50, y: 100))
+        bezierPath?.addLine(to: CGPoint(x: 270, y: 100))
+        bezierPath?.addLine(to: CGPoint(x: 270, y: 300))
+        bezierPath?.addLine(to: CGPoint(x: 50, y: 300))
+        bezierPath?.close()
+        UIColor.black.setStroke()
+        bezierPath?.stroke()
+    }
+
+    override func draw(_ rect: CGRect) {
+        
+        createShape()
+        
+        /// pen
+        path?.lineWidth = 8
+        UIColor.red.setStroke()
+        path?.stroke()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        ctr = 0;
+        
+        if let touch = touches.first {
+            
+            startPoint = touch.location(in: self)
+            pts[0] = startPoint
+            
+        }
+        super.touchesBegan(touches , with:event)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+           
+            let endPoint:CGPoint = touch.location(in: self)
+            
+            if(!bezierPath!.contains(endPoint))
+            {
+                delegate.fingerDidMoveToOutSideThePath(str: "outside")
+            }
+            else
+            {
+                delegate.fingerDidMoveToOutSideThePath(str: "inside")
+            }
+            
+            ctr += 1;
+            pts[ctr] = endPoint;
+            if (ctr == 4)
+            {
+                // move the endpoint to the middle of the line joining the second control point of the first Bezier segment and the first control point of the second Bezier segment
+                pts[3] = CGPoint(x: (pts[2]!.x + pts[4]!.x)/2.0, y: (pts[2]!.y + pts[4]!.y)/2.0);
+                path!.move(to: pts[0]!);
+                path?.addCurve(to: pts[3]!, controlPoint1: pts[1]!, controlPoint2: pts[2]!)
+                
+                self.setNeedsDisplay();
+                // replace points and get ready to handle the next segment
+                pts[0] = pts[3];
+                pts[1] = pts[4];
+                ctr = 1;
+            
+            }
+        super.touchesBegan(touches , with:event)
+        }
+  }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
---
+++ HelpTheKids/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelpTheKids
+//
+//  Created by Gerald Wood on 4/18/19.
+//  Copyright © 2019 Gerald Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,DrawingProtocol {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var drawingView: DrawingView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawingView.delegate = self
+    }
+    
+    func fingerDidMoveToOutSideThePath(str: String) {
+        myLabel.text = str;
+    }
+
+}
+
